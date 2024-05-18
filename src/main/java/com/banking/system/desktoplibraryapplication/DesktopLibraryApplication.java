fa5aca80b77feb9f1063fbd07d568b21ef4b366d
behuman <benassi22rayen@gmail.com>
2024-05-18 15:45:03 +0100

    first commit

---
+++ src/main/java/com/banking/system/desktoplibraryapplication/DesktopLibraryApplication.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.banking.system.desktoplibraryapplication;
+
+/**
+ *
+ * @author raben
+ */
+public class DesktopLibraryApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
