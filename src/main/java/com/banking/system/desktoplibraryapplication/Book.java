530d98d7bde0fa1da609568f820674a1ad3ee731
behuman <benassi22rayen@gmail.com>
2024-05-18 16:08:22 +0100

    adding getters and setters

--- src/main/java/com/banking/system/desktoplibraryapplication/Book.java
+++ src/main/java/com/banking/system/desktoplibraryapplication/Book.java
@@ -16,18 +16,55 @@ public class Book {
 	}
 
 	public void setNbPages(int nbPages) {
+		if(nbPages>0) {
 		this.nbPages = nbPages;
+		}
+		else {
+			System.err.println("nbPages should be > 0 !");
+		}
+		
 	}
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getNbPages() {
+		return nbPages;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
 
 	public static void main(String[] args) {
 		
 	Book book1= new Book("Welcome to the jungle","Rayen Benassi",9999);
 	Book book2= new Book("Do not be Evil","Olfa Elharzalli",10293);
 	
-	System.out.println("author of Book1: "+book1.getAuthor());
-	System.out.println("author of Book2: "+book2.getAuthor());
-		
+	
+	
+	System.out.println("*********Books : *********");
+	System.out.println(
+			"author of  "+ book1.getTitle() + ": "+
+	         book1.getAuthor() +"\n"+
+			 "number of pages: " +
+	         book1.getNbPages() 
+			);
+	
+	System.out.println(
+			"author of  "+ book2.getTitle() + ": "+
+	         book2.getAuthor() +"\n"+
+			 "number of pages: " +
+	         book2.getNbPages() 
+			);
+	
 
 	}
+
 
 }
