31f5c9355c15b66e3b245470107e54291e767686
behuman <benassi22rayen@gmail.com>
2024-05-18 16:58:15 +0100

    adding some logic

--- src/main/java/com/banking/system/desktoplibraryapplication/TestBooks.java
+++ src/main/java/com/banking/system/desktoplibraryapplication/TestBooks.java
@@ -4,24 +4,38 @@ import java.util.Scanner;
 
 public class TestBooks {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        Book[] books = new Book[3];
 
-		Book []books=new Book[3];
-	
-		for(int i =0 ;i<books.length;i++) {
-			Scanner scan = new Scanner(System.in);
-			books[i]=new Book();
-			System.out.println("complete info for book number :"+ (i+1));
-			System.out.println("Title : ?");
-			books[i].setTitle(scan.nextLine());
-			System.out.println("Athor : ?");
-			books[i].setAuthor(scan.nextLine());
-			System.out.println("nb of pages : ?");
-			books[i].setNbPages(Integer.parseInt(scan.nextLine()));
-			
-			
-		}
+        try (Scanner scan = new Scanner(System.in)) {  // Scanner declared here will be closed automatically
+            for (int i = 0; i < books.length; i++) {
+                books[i] = new Book();
+                System.out.println("Complete info for book number: " + (i + 1));
 
-	}
+                System.out.print("Title: ");
+                books[i].setTitle(scan.nextLine());
 
+                System.out.print("Author: ");
+                books[i].setAuthor(scan.nextLine());
+
+                System.out.print("Number of pages: ");
+                while (true) {
+                    try {
+                        books[i].setNbPages(Integer.parseInt(scan.nextLine()));
+                        break;
+                    } catch (NumberFormatException e) {
+                        System.out.print("Invalid input. Please enter a valid number of pages: ");
+                    }
+                }
+            }
+        } // Scanner is automatically closed here
+
+        // Optionally print out the book details to verify input
+        for (int i = 0; i < books.length; i++) {
+            System.out.println("\nBook " + (i + 1) + ":");
+            System.out.println("Title: " + books[i].getTitle());
+            System.out.println("Author: " + books[i].getAuthor());
+            System.out.println("Number of pages: " + books[i].getNbPages());
+        }
+    }
 }
